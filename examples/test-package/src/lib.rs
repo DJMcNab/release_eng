4daf607c340ff2c54547814584bd256165d86296
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2026-07-19 22:29:18 +1000

    Create a testing crate (#1)
    <!--
    Thanks for opening a PR for this Linebender project!
    -->
    
    <!-- If you used any LLM ("AI") tools in making this PR, please ensure
    that you have reviewed our
    LLM policy at https://linebender.org/wiki/llm-policy/.
    Please list any such tools you used (such as Claude Code or Codex)
    below.
    We expect you to have fully understood and self-reviewed your
    contributions to Linebender.
    
    If you did not use any LLM tools, please replace "Unspecified" with
    'None'. -->
    LLM Contributions: None.
    
    **Description**
    
    Creates a new test crate.
    
    <!--
    If this change has any user facing effects, please place a description
    of them in the following quote block.
    What you write here will be used as a basis for the changelog entry; it
    doesn't need to be perfect.
    If this change doesn't need a changelog entry, please replace the next
    line with `**Changelog: None**` and delete
    the quote block.
    -->
    **Changelog**
    
    > ### Added <!-- Or Fixed, Changed, etc. -->
    >
    > - The 'greeting` function.

---
+++ examples/test-package/src/lib.rs
@@ -0,0 +1,6 @@
+//! A package used to demonstrate the release engineering in this repository.
+
+/// Returns an arbitrary English language greeting.
+pub fn greeting() -> &'static str {
+    "Hello, world!"
+}
